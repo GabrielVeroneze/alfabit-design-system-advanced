85276129cc5f03e4310d949690f9c50680e5d8ad
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-11-14 18:00:19 -0300

    ♻️ refactor: migra Typography para uso de class-variance-authority (CVA)
    Refatora o componente Typography para utilizar a biblioteca `class-variance-authority `, removendo o antigo mapeamento manual de classes e centralizando a lógica de variantes em uma única função.

--- components/Typography/Typography.tsx
+++ components/Typography/Typography.tsx
@@ -1,3 +1,31 @@
+import { cva } from 'class-variance-authority'
+
+const textVariants = cva('text-gray-primary font-normal', {
+    variants: {
+        variant: {
+            primary: 'text-gray-primary',
+            secondary: 'text-gray-secondary',
+            tertiary: 'text-gray-tertiary',
+        },
+        size: {
+            xs: 'text-xs',
+            sm: 'text-sm',
+            md: 'text-md',
+            lg: 'text-lg',
+            xl: 'text-xl',
+            title1: 'text-title-xl',
+            title2: 'text-title-lg',
+            title3: 'text-title-md',
+            subtitle1: 'text-subtitle-lg',
+            subtitle2: 'text-subtitle-md',
+        },
+    },
+    defaultVariants: {
+        variant: 'primary',
+        size: 'md',
+    },
+})
+
 export interface TypographyProps extends React.HTMLAttributes<HTMLElement> {
     children: React.ReactNode
     element?: 'div' | 'span' | 'p' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
@@ -15,44 +43,16 @@ export interface TypographyProps extends React.HTMLAttributes<HTMLElement> {
         | 'subtitle2'
 }
 
-const variantClassMap = {
-    variant: {
-        primary: 'text-gray-primary',
-        secondary: 'text-gray-secondary',
-        tertiary: 'text-gray-tertiary',
-    },
-    size: {
-        xs: 'text-xs',
-        sm: 'text-sm',
-        md: 'text-md',
-        lg: 'text-lg',
-        xl: 'text-xl',
-        title1: 'text-title-xl',
-        title2: 'text-title-lg',
-        title3: 'text-title-md',
-        subtitle1: 'text-subtitle-lg',
-        subtitle2: 'text-subtitle-md',
-    },
-}
-
 const Typography = ({
     children,
     element = 'p',
-    variant = 'primary',
-    size = 'md',
+    className,
     ...rest
 }: TypographyProps) => {
     const ElementTag = element
 
     return (
-        <ElementTag
-            className={`
-                text-gray-primary font-normal
-                ${variantClassMap.variant[variant]}
-                ${variantClassMap.size[size]}
-            `}
-            {...rest}
-        >
+        <ElementTag className={`${textVariants(rest)} ${className}`} {...rest}>
             {children}
         </ElementTag>
     )
