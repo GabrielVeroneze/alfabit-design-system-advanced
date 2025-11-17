3774bc91de3658a715e0207382d924e1ccb0bf87
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-11-17 15:43:57 -0300

    ✨ feat: adiciona estado interno, ícones e suporte aos variants common e contract
    Aprimora o componente Switch adicionando gerenciamento interno de estado, suporte ao toggle, propagação do onChange e controle de disabled. Inclui exibição condicional de ícones (CheckIcon e XMarkIcon) para o variant "contract", mantendo um placeholder para o variant "common". O componente agora
    é totalmente controlado internamente, com possibilidade de integração externa.

--- components/Switch/Switch.tsx
+++ components/Switch/Switch.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+import { CheckIcon, XMarkIcon } from '@heroicons/react/24/solid'
 import {
     Switch as HeadlessSwitch,
     SwitchProps as HeadlessSwitchProps,
@@ -15,8 +17,39 @@ const Switch = ({
     variant = 'common',
     disabled,
     onChange,
+    ...rest
 }: SwitchProps) => {
-    return <HeadlessSwitch>Switch</HeadlessSwitch>
+    const [enabled, setEnabled] = useState(enabledByDefault)
+
+    const toggle = () => {
+        const newState = !enabled
+
+        setEnabled(newState)
+
+        if (onChange) {
+            onChange(newState)
+        }
+    }
+
+    return (
+        <HeadlessSwitch
+            checked={enabled}
+            onChange={toggle}
+            disabled={disabled}
+            {...rest}
+        >
+            {variant === 'common' && <span></span>}
+            {variant === 'contract' && (
+                <span>
+                    {enabled ? (
+                        <CheckIcon aria-disabled={disabled} />
+                    ) : (
+                        <XMarkIcon aria-disabled={disabled} />
+                    )}
+                </span>
+            )}
+        </HeadlessSwitch>
+    )
 }
 
 export default Switch
