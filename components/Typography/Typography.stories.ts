84d7ca44e8836e6403c62b795edbec0755219959
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-11-18 17:12:53 -0300

    ✨ feat: adiciona novas variações de stories para headings e parágrafo
    Substitui o story Primary por Default e adiciona novas variações para o componente Text, incluindo H1, H2, H3 e parágrafo. Cada variação define elemento, tamanho e estilos específicos, permitindo maior cobertura e visualização das opções disponíveis no Storybook.

--- components/Typography/Typography.stories.ts
+++ components/Typography/Typography.stories.ts
@@ -15,8 +15,44 @@ export default meta
 
 type Story = StoryObj<typeof meta>
 
-export const Primary: Story = {
+export const Default: Story = {
     args: {
         children: 'Texto',
+    },
+}
+
+export const TextHeadingH1: Story = {
+    args: {
+        children: 'Texto',
+        element: 'h1',
+        size: 'title1',
+        className: 'font-extrabold',
+    },
+}
+
+export const TextHeadingH2: Story = {
+    args: {
+        children: 'Texto',
+        element: 'h2',
+        size: 'title2',
+        className: 'font-extrabold',
+    },
+}
+
+export const TextHeadingH3: Story = {
+    args: {
+        children: 'Texto',
+        element: 'h3',
+        size: 'title3',
+        className: 'font-extrabold',
+    },
+}
+
+export const TextParagraph: Story = {
+    args: {
+        children: 'Texto',
+        element: 'p',
+        size: 'md',
+        className: 'text-gray-primary',
     },
 }
