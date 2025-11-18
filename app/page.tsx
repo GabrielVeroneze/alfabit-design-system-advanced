b3d3e20f162bc2dab807e3d3cbbeee6894d4f816
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-11-18 16:45:47 -0300

    ♻️ refactor: substitui Button por Switch e ajustar layout da página
    Remove o componente Button do App e adiciona o componente Switch em seu lugar. Também aplica uma estrutura de layout centralizada usando Flexbox
    para facilitar a visualização do componente durante o desenvolvimento.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
-import Button from '@/components/Button/Button'
+import Switch from '@/components/Switch/Switch'
 
 const App = () => {
-    return <Button>Botão</Button>
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <div className="flex flex-col gap-1">
+                <Switch />
+            </div>
+        </div>
+    )
 }
 
 export default App
